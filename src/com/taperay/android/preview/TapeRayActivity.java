818e60d86e2345897b6a0b532b34158a55361e1d
Daniel Mack <zonque@gmail.com>
2012-05-16 19:48:30 +0200

    added default menu with 'about taperay' info screen

--- src/com/taperay/android/preview/TapeRayActivity.java
+++ src/com/taperay/android/preview/TapeRayActivity.java
@@ -3,11 +3,19 @@ package com.taperay.android.preview;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class TapeRayActivity extends Activity {
 
 	protected void displayNetworkErrorAndFinish() {
-
         runOnUiThread(new Runnable() {
             public void run() {
         		AlertDialog.Builder builder = new AlertDialog.Builder(TapeRayActivity.this);  
@@ -25,5 +33,44 @@ public class TapeRayActivity extends Activity {
                 alert.show();
             }
         });
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.default_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {		
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.about_taperay:
+			final TextView message = new TextView(TapeRayActivity.this);
+			final SpannableString s = 
+				new SpannableString(getResources().getString(R.string.about_taperay_message));
+			Linkify.addLinks(s, Linkify.WEB_URLS);
+			message.setText(s);
+			message.setMovementMethod(LinkMovementMethod.getInstance());
+			
+    		AlertDialog.Builder builder = new AlertDialog.Builder(TapeRayActivity.this);  
+            builder.setTitle(getResources().getString(R.string.about_taperay));  
+            builder.setView(message);  
+            builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {  
+                 @Override
+                 public void onClick(DialogInterface dialog, int which) {  
+                     dialog.cancel();  
+                     finish();
+                 }  
+            });
+
+            AlertDialog alert = builder.create(); 
+            alert.show();
+
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }

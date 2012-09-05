82da4ba4c55704fec46db8617b6683d640324e12
Daniel Mack <zonque@gmail.com>
2012-09-05 22:38:02 +0200

    TouchImageView: strip some dead code

--- src/com/taperay/android/preview/TouchImageView.java
+++ src/com/taperay/android/preview/TouchImageView.java
@@ -147,43 +147,8 @@ public class TouchImageView extends ImageView {
 			bottom = height * saveScale - height - (2 * redundantYSpace * saveScale);
 			if (origWidth * saveScale <= width || origHeight * saveScale <= height) {
 				matrix.postScale(mScaleFactor, mScaleFactor, width / 2, height / 2);
-				/*
-				if (mScaleFactor < 1) {
-					matrix.getValues(m);
-					float x = m[Matrix.MTRANS_X];
-					float y = m[Matrix.MTRANS_Y];
-					if (mScaleFactor < 1) {
-						if (Math.round(origWidth * saveScale) < width) {
-							if (y < -bottom)
-								matrix.postTranslate(0, -(y + bottom));
-							else if (y > 0)
-								matrix.postTranslate(0, -y);
-						} else {
-							if (x < -right)
-								matrix.postTranslate(-(x + right), 0);
-							else if (x > 0)
-								matrix.postTranslate(-x, 0);
-						}
-					}
-				}
-				 */
 			} else {
 				matrix.postScale(mScaleFactor, mScaleFactor, detector.getFocusX(), detector.getFocusY());
-				/*
-				matrix.getValues(m);
-				float x = m[Matrix.MTRANS_X];
-				float y = m[Matrix.MTRANS_Y];
-				if (mScaleFactor < 1) {
-					if (x < -right)
-						matrix.postTranslate(-(x + right), 0);
-					else if (x > 0)
-						matrix.postTranslate(-x, 0);
-					if (y < -bottom)
-						matrix.postTranslate(0, -(y + bottom));
-					else if (y > 0)
-						matrix.postTranslate(0, -y);
-				}
-				 */
 			}
 			return true;
 
